b0e7c127c858e9b6b94d4e3ce8deeefc95b72f6c
JurgenHahn <jurgenehahn@gmail.com>
2017-02-02 16:47:28 -0500

    added validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,14 @@
 class Image < ApplicationRecord
 
+  validates :artist, presence: true
+
+  validates :title, length: { in: 3..20 }
+
+  validates :url, presence: true,
+                  uniqueness: true
+
+  validates :url, :format => URI::regexp(%w(http https))
+  
   def self.newest_first
     Image.order("created_at DESC")
   end
